c0bf739ecf72f1620de2b3be5856b5a27a161f16
Paulo Duarte <phduarte87@outlook.com>
2018-01-19 22:33:04 -0200

    Publicação inicial

--- Gadz.Tetris.Model/Pecas/Formas/IForma.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Gadz.Tetris.Model.Pecas.Formas {
-    public interface IForma {
-        int Rotacao { get; }
-        Bloco[] Blocos { get; }
-    }
-}
--- Gadz.Tetris.Model/Pecas/IMovil.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Gadz.Tetris.Model.Pecas {
-    public interface IMovil {
-        void MoverEsquerda();
-        void MoverDireita();
-        void MoverBaixo();
-        void Girar();
-    }
-}
